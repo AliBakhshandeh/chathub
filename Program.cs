43f88208834b6c08def263e6179db5da15cba7e3
Bakhshandeh Ardestani, Ali (TPD) <bakhshandeh.ali@tapsi.doctor>
2025-07-08 20:23:15 +0330

    test1

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("http://localhost:4000")
+        policy.WithOrigins("localhost:4000")
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials();
